cb56ff5686681341f90fc158a5027003b2d66a1b
JoseMBNeto <miltoneto4231@gmail.com>
2026-08-28 18:18:36 -0300

    Correção de Bug e primeira evidência

--- base_mandelbrot.c
+++ base_mandelbrot.c
@@ -6,7 +6,7 @@ double pixelParaReal (int coluna, int largura){
     const double minimo = -2.0;
     const double maximo = 1.0;
 
-    double fracao = (double) coluna / (double) largura - 1;
+    double fracao = (double) coluna / ((double) largura - 1);
     double valor = minimo + fracao * (maximo - minimo);
     return valor;
 }
@@ -15,7 +15,7 @@ double pixelParaImagem (int linha, int altura){
     const double minimo = -1.5;
     const double maximo = 1.5;
 
-    double fracao = (double) linha / (double) altura - 1;
+    double fracao = (double) linha / ((double) altura -1);
     double valor = minimo + fracao * (maximo - minimo);
     return valor;
 }
